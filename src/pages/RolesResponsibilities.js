c1944ba96cd7131197bc09247240651d19a92371
Sagar R. Kothari <sag333ar@gmail.com>
2021-09-12 07:00:40 +0530

    added responsibilities page

--- src/pages/RolesResponsibilities.js
+++ src/pages/RolesResponsibilities.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./RolesResponsibilities.css";
 
 const RolesResponsibilities = () => {
   return (
-    <div>
-      <h1>Roles and Responsibilities</h1>
-      <p>
-        The following are the roles and responsibilities.
-      </p>
-      </div>
-  )
-}
+    <div className="roles">
+      <h3>Key Responsibilities</h3>
+      <h4>Project Management</h4>
+      <ul>
+        <li>
+          Communication with Project Manager, Solution Architect &amp; Client
+        </li>
+        <li>Providing estimates, technical documents</li>
+        <li>Defining scopes</li>
+        <li>Tracking progress of each task</li>
+        <li>Preparing release &amp; sharing with client</li>
+      </ul>
+      <h4>Technical Responsibilities</h4>
+      <ul>
+        <li>App build &amp; distribution</li>
+        <li>Code review</li>
+        <li>Merging, reviewing, resolving code conflicts</li>
+        <li>Developing mockups for Apps</li>
+        <li>Describing development requirements to developers</li>
+        <li>Assigning tasks</li>
+        <li>Support on technical challenges</li>
+        <li>Research and development</li>
+        <li>Scheduling &amp; taking Knowledge sharing sessions</li>
+        <li>Writing Unit test cases &amp; UI Test cases</li>
+        <li>Application developer level testing</li>
+      </ul>
+    </div>
+  );
+};
 
 export default RolesResponsibilities;
